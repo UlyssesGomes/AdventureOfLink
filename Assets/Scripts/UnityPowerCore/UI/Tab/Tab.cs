7511fe5bf21b1379eb029c677fa3c903d69b50ca
Ulysses <ugomes089@gmail.com>
2023-11-05 00:32:33 -0300

    feat: Building panel background imagens fixed; Tab script implemnetation; Build panel sizes, positions and anchors fixed.

---
+++ Assets/Scripts/UnityPowerCore/UI/Tab/Tab.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Tab : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite selectedSprite;
+    [SerializeField]
+    private Sprite unselectedSprite;
+
+    [SerializeField]
+    private Color unselectedColor;
+    
+    [SerializeField]
+    private Image image;
+
+    public Text text;
+
+    private void Start()
+    {
+        // image.sprite = unselectedSprite;
+    }
+
+    public void select(bool doSelect)
+    {
+        if (doSelect)
+            image.sprite = selectedSprite;
+        else
+        {
+            image.sprite = unselectedSprite;
+            image.color = unselectedColor;
+        }
+    }
+}
