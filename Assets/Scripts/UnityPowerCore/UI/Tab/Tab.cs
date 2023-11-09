e7cbea1ef475aa6140125fbcdb0e0816b10344fe
Ulysses <ugomes089@gmail.com>
2023-11-09 01:12:40 -0300

    feat: DescriptionPanel ui enhancements; DescriptionSlot prefab implementation; ItemImage prefab implementation; MaterialItemImage prefab implementation; TextNumber prefab implementation;

--- Assets/Scripts/UnityPowerCore/UI/Tab/Tab.cs
+++ Assets/Scripts/UnityPowerCore/UI/Tab/Tab.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Tab : MonoBehaviour
+public class Tab : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler
 {
     [SerializeField]
     private Sprite selectedSprite;
@@ -31,4 +32,19 @@ public class Tab : MonoBehaviour
             image.color = unselectedColor;
         }
     }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Clicou no GameObject: " + gameObject.name);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Entrou no GameObject: " + gameObject.name);
+    }
+    //private void OnMouseDown()
+    //{
+    //    // Coloque aqui o código a ser executado quando o GameObject for clicado.
+    //    Debug.Log("Clicou no GameObject: " + gameObject.name);
+    //}
 }
