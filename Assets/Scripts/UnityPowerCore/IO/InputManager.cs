5663d706e3f3c8c710ff002819c5e154db271efb
Ulysses <ugomes089@gmail.com>
2023-12-02 01:45:43 -0300

    feat: InputManager initial implementation.

---
+++ Assets/Scripts/UnityPowerCore/IO/InputManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class InputManager<T>: Input where T : struct
+{
+    public static T allowedControllingAgent;
+
+    public static float GetAxis(string axisName, T currentAgent)
+    {
+        float magniture = Input.GetAxis(axisName);
+        if (magniture != 0.0f && currentAgent.Equals(allowedControllingAgent))
+        {
+            return magniture;
+        }
+
+        return 0.0f;
+    }
+
+    public static float GetAxisRaw(string axisName, T currentAgent)
+    {
+        float magniture = Input.GetAxisRaw(axisName);
+        if (magniture != 0.0f && currentAgent.Equals(allowedControllingAgent))
+        {
+            return magniture;
+        }
+
+        return 0.0f;
+    }
+
+    public static bool GetButton(string buttonName, T currentAgent)
+    {
+
+        if(Input.GetButton(buttonName) && currentAgent.Equals(allowedControllingAgent))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static bool GetButtonDown(string buttonName, T currentAgent)
+    {
+        if (Input.GetButtonDown(buttonName) && currentAgent.Equals(allowedControllingAgent))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static bool GetButtonUp(string buttonName, T currentAgent)
+    {
+        if (Input.GetButtonUp(buttonName) && currentAgent.Equals(allowedControllingAgent))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+}
