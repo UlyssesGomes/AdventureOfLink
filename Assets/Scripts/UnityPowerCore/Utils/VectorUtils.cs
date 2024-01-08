74ec0b0126db8f3b32a4043eaaebe42b4165eeb5
Ulysses <ugomes089@gmail.com>
2024-01-08 02:27:35 -0300

    feat: VectorUtils angle equations and unit tests implementation.

--- Assets/Scripts/UnityPowerCore/Utils/VectorUtils.cs
+++ Assets/Scripts/UnityPowerCore/Utils/VectorUtils.cs
@@ -2,28 +2,63 @@
 
 public static class VectorUtils 
 {
-    public static float angleBetweenPoints2(Vector2 origin, Vector2 target)
+
+    /// <summary>
+    /// Calculate angle between two points that represents a Vector.
+    /// </summary>
+    /// <param name="origin">Origin point as Vector3</param>
+    /// <param name="dest">Destination point as Vector3</param>
+    /// <returns>Angle in degrees</returns>
+    public static float angleBetweenPoints3(Vector3 origin, Vector3 dest)
     {
-        float theta = 0;
+        Vector3 vector = dest - origin;
+
+        float theta = Mathf.Atan2(vector.y, vector.x);
+        theta *= Mathf.Rad2Deg;
+
+        // Cartesian Q3 and Q4
+        if ((vector.x < 0 && vector.y < 0) || (vector.x >= 0 && vector.y < 0))
+        {
+            theta += 360.0f;
+        }
+
         return theta;
     }
 
-    public static float angleBetweenPoints3(Vector3 origin, Vector3 target)
+    /// <summary>
+    /// Calculate a angle in a Vector3 which origin is in (0, 0, 0)
+    /// </summary>
+    /// <param name="vector">Vector3</param>
+    /// <returns>Angle in degrees</returns>
+    public static float angleInVector3(Vector3 vector)
     {
-        Vector3 vector = target - origin;
+        float theta = Mathf.Atan2(vector.y, vector.x);
+        theta *= Mathf.Rad2Deg;
 
-        float theta = 0;
+        // Cartesian Q3 and Q4
+        if ((vector.x < 0 && vector.y < 0) || (vector.x >= 0 && vector.y < 0))
+        {
+            theta += 360.0f;
+        }
 
-        theta = Mathf.Atan2(vector.y, vector.x);
-
-        return theta * Mathf.Rad2Deg;
+        return theta;
     }
 
+    /// <summary>
+    /// Transform angle from radians to degrees
+    /// </summary>
+    /// <param name="radians">Value in radians</param>
+    /// <returns>Values in degrees</returns>
     public static float radiansToDegrees(float radians)
     {
         return Mathf.Rad2Deg * radians;
     }
 
+    /// <summary>
+    /// Transform angle from degrees to radians
+    /// </summary>
+    /// <param name="degrees">Value in degrees</param>
+    /// <returns>Value in radians</returns>
     public static float degreesToRadians(float degrees)
     {
         return Mathf.Deg2Rad * degrees;
--- Assets/UnitTests/VectorUtilsTest.cs
+++ Assets/UnitTests/VectorUtilsTest.cs
@@ -8,6 +8,8 @@ namespace Tests
 {
     public class VectorUtilsTest
     {
+        // ========================= Vector3 =================================
+
         [Test]
         public void TestVector3With90Degrees()
         {
@@ -49,19 +51,147 @@ namespace Tests
 
             float theta = VectorUtils.angleBetweenPoints3(origin, target);
 
-            Assert.AreEqual(254.054596f, theta + 360.0f);
+            Assert.AreEqual(254.054596f, theta);
         }
 
         [Test]
-        public void TestVector3With360Degrees()
+        public void TestVector3With293Degrees()
         {
             Vector3 origin = Vector3.zero;
             Vector3 target = new Vector3(3, -7);
 
             float theta = VectorUtils.angleBetweenPoints3(origin, target);
 
-            Assert.AreEqual(293.198608f, theta + 360);
+            Assert.AreEqual(293.198608f, theta);
         }
+
+        [Test]
+        public void TestSingleVector3With45Degrees()
+        {
+            Vector3 target = new Vector3(6, 6);
+
+            float theta = VectorUtils.angleInVector3(target);
+
+            Assert.AreEqual(45f, theta);
+        }
+
+        [Test]
+        public void TestSingleVector3With146Degrees()
+        {
+            Vector3 target = new Vector3(-3, 2);
+
+            float theta = VectorUtils.angleInVector3(target);
+
+            Assert.AreEqual(146.309937f, theta);
+        }
+
+        [Test]
+        public void TestSingleVector3With254Degrees()
+        {
+            Vector3 target = new Vector3(-2, -7);
+
+            float theta = VectorUtils.angleInVector3(target);
+
+            Assert.AreEqual(254.054596f, theta);
+        }
+
+        [Test]
+        public void TestSingleVector3With293Degrees()
+        {
+            Vector3 target = new Vector3(3, -7);
+
+            float theta = VectorUtils.angleInVector3(target);
+
+            Assert.AreEqual(293.198608f, theta);
+        }
+
+        // ========================= Vector2 =================================
+
+        [Test]
+        public void TestVector2With45Degrees()
+        {
+            Vector2 origin = Vector2.zero;
+            Vector2 target = new Vector2(6, 6);
+
+            float theta = VectorUtils.angleBetweenPoints3(origin, target);
+
+            Assert.AreEqual(45f, theta);
+        }
+
+        [Test]
+        public void TestVector2With146Degrees()
+        {
+            Vector2 origin = Vector2.zero;
+            Vector2 target = new Vector2(-3, 2);
+
+            float theta = VectorUtils.angleBetweenPoints3(origin, target);
+
+            Assert.AreEqual(146.309937f, theta);
+        }
+
+        [Test]
+        public void TestVector2With254Degrees()
+        {
+            Vector2 origin = Vector2.zero;
+            Vector2 target = new Vector2(-2, -7);
+
+            float theta = VectorUtils.angleBetweenPoints3(origin, target);
+
+            Assert.AreEqual(254.054596f, theta);
+        }
+
+        [Test]
+        public void TestVector2With293Degrees()
+        {
+            Vector2 origin = Vector2.zero;
+            Vector2 target = new Vector2(3, -7);
+
+            float theta = VectorUtils.angleBetweenPoints3(origin, target);
+
+            Assert.AreEqual(293.198608f, theta);
+        }
+
+        [Test]
+        public void TestSingleVector2With45Degrees()
+        {
+            Vector2 target = new Vector2(6, 6);
+
+            float theta = VectorUtils.angleInVector3(target);
+
+            Assert.AreEqual(45f, theta);
+        }
+
+        [Test]
+        public void TestSingleVector2With146Degrees()
+        {
+            Vector2 target = new Vector2(-3, 2);
+
+            float theta = VectorUtils.angleInVector3(target);
+
+            Assert.AreEqual(146.309937f, theta);
+        }
+
+        [Test]
+        public void TestSingleVector2With254Degrees()
+        {
+            Vector2 target = new Vector2(-2, -7);
+
+            float theta = VectorUtils.angleInVector3(target);
+
+            Assert.AreEqual(254.054596f, theta);
+        }
+
+        [Test]
+        public void TestSingleVector2With293Degrees()
+        {
+            Vector2 target = new Vector2(3, -7);
+
+            float theta = VectorUtils.angleInVector3(target);
+
+            Assert.AreEqual(293.198608f, theta);
+        }
+
+        // ========================= Mathf.Atan =================================
 
         [Test]
         public void TestingAtan1Q3()
